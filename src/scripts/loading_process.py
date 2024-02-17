971a0d6395154745042d75ae501d7b64c1d7964e
SamahaAbbas <127323854+SamahaAbbas@users.noreply.github.com>
2024-02-17 03:02:03 +0200

    loading_process.py

--- src/scripts/loading_process.py
+++ src/scripts/loading_process.py
@@ -1,7 +1,7 @@
 
 
 def loading_process_part(index):
-	animation_frames = ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"]
-	index_frame = index % len(animation_frames)
-	process_string = "{ info }" + " loading data from result data " + animation_frames[index_frame]
-	return process_string
+        animation_frames = ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"]
+        index_frame = index % len(animation_frames)
+        process_string = "{ info }" + " " + animation_frames[index_frame]
+        return process_string
